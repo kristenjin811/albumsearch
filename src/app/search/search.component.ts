f43bde60ddc8620c4b89aa68013d2275d3c8e086
kristenjin811 <97808382+kristenjin811@users.noreply.github.com>
2023-06-05 17:14:09 -0700

    Initial commit

---
+++ src/app/search/search.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+import { products } from '../products';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css']
+})
+export class SearchComponent {
+  products = [...products];
+
+  share() {
+    window.alert('The product has been shared!');
+  }
+}
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
